c5595416fb7097dcd122cd4c255c1065da8868da
arun-murali0 <arunkarthick0810@gmail.com>
2024-10-09 16:27:12 +0530

    navbar ui updated

--- src/view/Navbar.tsx
+++ src/view/Navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 const Navbar = async () => {
   return (
-    <header className="px-4 h-14 sticky top-0 inset-x-0 w-full bg-background/40 backdrop-blur-lg  z-50">
+    <header className="px-4 h-20 sticky top-0 inset-x-0 w-full bg-background/40 backdrop-blur-lg  z-50">
       <Container reverse>
         <div className="flex items-center justify-between h-full mx-auto md:max-w-screen-xl">
           <div className="flex items-start">
